0b28622b6732a40d0bdef5c68f46970cb0862dbd
sancarbar <sancabar@gmail.com>
2023-01-16 20:58:31 -0500

    1. Initial project from Spring Boot Intilzr

---
+++ src/main/kotlin/org/adaschool/microservicesspringboottests/MicroservicesSpringBootTestsApplication.kt
@@ -0,0 +1,11 @@
+package org.adaschool.microservicesspringboottests
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroservicesSpringBootTestsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicroservicesSpringBootTestsApplication>(*args)
+}
---
+++ src/test/kotlin/org/adaschool/microservicesspringboottests/MicroservicesSpringBootTestsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.adaschool.microservicesspringboottests
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroservicesSpringBootTestsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
